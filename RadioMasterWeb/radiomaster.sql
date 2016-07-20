0824a4c8bc725660cfa9a37ac95971e4a649fadd
SlavenKarakas <skarakas84@gmail.com>
2016-07-20 10:50:04 +0200

    dodano

--- RadioMasterWeb/radiomaster.sql
+++ RadioMasterWeb/radiomaster.sql
@@ -18,4 +18,26 @@ create unique index ix1 on user(username);
 insert into user(id,username,email,password,created_at,updated_at) values 
 (1,'npepic','npepic@gmail.com',md5('n'),'2016-07-18 06:00:00','2016-07-19 06:00:00');
 
+create table countries (
+id int not null primary key auto_increment,
+name varchar(50) not null,
+country_code varchar(50) not null,
+region varchar (50) not null,
+subregion varchar (50) not null,
+flag_img_url varchar (50) not null
+);
+
+create table categories (
+id int not null primary key auto_increment,
+title varchar(50) not null,
+description varchar(50) not null,
+created_at datetime
+);
+
+create table stations (
+id int not null primary key auto_increment,
+rating varchar(50) not null,
+name varchar(50) not null,
+updated_at datetime
+);
 
